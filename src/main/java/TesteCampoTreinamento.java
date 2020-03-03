a02b344a383b9791bcf3ed325de6c75129106cff
msilvcri <matheus.scristino@gmail.com>
2020-03-03 09:54:01 -0300

    Adicionando site do treinamento

--- src/main/java/TesteCampoTreinamento.java
+++ src/main/java/TesteCampoTreinamento.java
@@ -1,16 +1,14 @@
-import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class TesteGoogle {
-
+public class TesteCampoTreinamento {
     @Test
-    public void teste(){
+    public void Teste(){
         WebDriver driver = new FirefoxDriver();
         driver.manage().window().maximize();
-        driver.get("https://www.google.com");
-        Assert.assertEquals("Google",driver.getTitle());
-        driver.quit();
+        driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+
+        //driver.quit();
     }
 }
