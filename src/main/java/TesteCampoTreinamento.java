9684683a4e1e2bae84a415113ae341ade77b14b0
msilvcri <matheus.scristino@gmail.com>
2020-03-03 10:56:28 -0300

    Métodos de interação com combobox

--- src/main/java/TesteCampoTreinamento.java
+++ src/main/java/TesteCampoTreinamento.java
@@ -2,7 +2,11 @@ import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.List;
 
 public class TesteCampoTreinamento {
 
@@ -45,6 +49,38 @@ public class TesteCampoTreinamento {
         driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
         driver.findElement(By.id("elementosForm:comidaFavorita:0")).click();
         Assert.assertTrue(driver.findElement(By.id("elementosForm:comidaFavorita:0")).isSelected());
+        driver.quit();
+    }
+
+    @Test
+    public void deveInteragirComComboBox(){
+        WebDriver driver = new FirefoxDriver();
+        driver.manage().window().maximize();
+        driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+        WebElement elemento =  driver.findElement(By.id("elementosForm:escolaridade"));
+        Select select = new Select(elemento);
+        List<WebElement> option = select.getOptions();
+        Assert.assertEquals(8,option.size());
+
+        boolean encontrou = false;
+        for (WebElement options:option){
+            if (options.getText().equals("Mestrado"));
+            encontrou = true;
+            break;
+        }
+        Assert.assertTrue(encontrou);
+        driver.quit();
+    }
+
+    @Test
+    public void verificarValoresDisponiveisNoCombo(){
+        WebDriver driver = new FirefoxDriver();
+        driver.manage().window().maximize();
+        driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+        WebElement elemento =  driver.findElement(By.id("elementosForm:escolaridade"));
+        Select select = new Select(elemento);
+        select.selectByVisibleText("2o grau incompleto");
+        Assert.assertEquals("2o grau incompleto",select.getFirstSelectedOption().getText());
         driver.quit();
     }
 
