2a9897c6686171e5c81d71a0d368a525f7fccea5
msilvcri <matheus.scristino@gmail.com>
2020-03-03 10:42:55 -0300

    Métodos de interação com checkbox e radioButton

--- src/main/java/TesteCampoTreinamento.java
+++ src/main/java/TesteCampoTreinamento.java
@@ -1,14 +1,51 @@
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class TesteCampoTreinamento {
+
     @Test
-    public void Teste(){
+    public void testeTextField(){
         WebDriver driver = new FirefoxDriver();
         driver.manage().window().maximize();
         driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
-
-        //driver.quit();
+        driver.findElement(By.id("elementosForm:nome")).sendKeys("Matheus");
+        Assert.assertEquals("Matheus",driver.findElement(By.id("elementosForm:nome")).getAttribute("value"));
+        driver.findElement(By.id("elementosForm:sobrenome")).sendKeys("Cristino");
+        Assert.assertEquals("Cristino",driver.findElement(By.id("elementosForm:sobrenome")).getAttribute("value"));
+        driver.quit();
     }
+
+    @Test
+    public void deveInteragirComTextArea(){
+        WebDriver driver = new FirefoxDriver();
+        driver.manage().window().maximize();
+        driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+        driver.findElement(By.id("elementosForm:sugestoes")).sendKeys("Sem sugestão");
+        Assert.assertEquals("Sem sugestão",driver.findElement(By.id("elementosForm:sugestoes")).getAttribute("value"));
+        driver.quit();
+    }
+
+    @Test
+    public void deveInteragirComRadioButton(){
+        WebDriver driver = new FirefoxDriver();
+        driver.manage().window().maximize();
+        driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+        driver.findElement(By.id("elementosForm:sexo:0")).click();
+        Assert.assertTrue(driver.findElement(By.id("elementosForm:sexo:0")).isSelected());
+        driver.quit();
+    }
+
+    @Test
+    public void deveInteragirComCheckBox(){
+        WebDriver driver = new FirefoxDriver();
+        driver.manage().window().maximize();
+        driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+        driver.findElement(By.id("elementosForm:comidaFavorita:0")).click();
+        Assert.assertTrue(driver.findElement(By.id("elementosForm:comidaFavorita:0")).isSelected());
+        driver.quit();
+    }
+
 }
